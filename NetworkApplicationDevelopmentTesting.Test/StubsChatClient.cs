6751948c197fe45727cb729f784cb5c8765fe9f2
Dmitry Arzhevitin <adachel@yandex.ru>
2024-05-06 18:17:18 +0500

    temp6

---
+++ NetworkApplicationDevelopmentTesting.Test/StubsChatClient.cs
@@ -0,0 +1,28 @@
+﻿using ChatApp;
+using ChatNetWork;
+using CommonChat.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkApplicationDevelopmentTesting.Test
+{
+    public class StubsChatClient : ChatClient
+    {
+        private ChatMessage _chatMessage = new ChatMessage();
+        public StubsChatClient(string name, IMessageSource messageSource) : base(name, messageSource)
+        {
+            _chatMessage.FromName = name;
+            //_chatMessage.ToName = 
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ NetworkApplicationDevelopmentTesting.Test/UnitTestClient.cs
@@ -0,0 +1,51 @@
+﻿using ChatApp;
+using ChatBD;
+using ChatNetWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkApplicationDevelopmentTesting.Test
+{
+    public class UnitTestClient
+    {
+        private ChatServer _server = new ChatServer(new MessageSource());
+        private int _port = 0;
+        private string _address = "127.0.0.1";
+
+        [SetUp]
+        public void Setup()
+        {
+            _server.clients.Clear();
+        }
+
+        [Test] // регистрация на сервере
+        public void Test1()
+        {
+            Task.Run(() => _server.WorkAsync() ).Wait(1000);
+
+            var сlient = new ChatClient("Alex", new MessageSource(_port, _address));
+            Task.Run(() => сlient.Start()).Wait(1000);
+
+            Task.Run(() => Assert.IsTrue(_server.clients.Count == 1)).Wait(1000);
+        }
+
+
+        [Test] // 
+        public void Test2()
+        {
+            Task.Run(() => _server.WorkAsync()).Wait(1000);
+
+
+        }
+
+
+        [TearDown]
+        public void Teardown()
+        {
+            _server.clients.Clear();
+        }
+    }
+}
