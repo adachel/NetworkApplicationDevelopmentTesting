3a4926446879c205d458b7eb8b9700f5bdd0b88c
Dmitry Arzhevitin <adachel@yandex.ru>
2024-05-08 01:01:26 +0500

    temp7

---
+++ NetworkApplicationDevelopmentTesting.Test/FakeChatClient.cs
@@ -0,0 +1,79 @@
+﻿using ChatNetWork;
+using CommonChat.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkApplicationDevelopmentTesting.Test
+{
+    public class FakeChatClient
+    {
+        private readonly string _name;
+        private readonly IMessageSource _messageSource;
+        private readonly IPEndPoint _serverEndPoint;
+
+        public string ToName { get; set; } = "";
+        public string Text { get; set; } = "";
+
+        public FakeChatClient(string name, IMessageSource messageSource)
+        {
+            _name = name;
+            _messageSource = messageSource;
+            _serverEndPoint = messageSource.GetServerIPEndPoint();
+        }
+
+        public void SendMessage(ChatMessage chatMessage, IPEndPoint ip)
+        {
+            _messageSource.SendMessage(chatMessage, ip);
+        }
+
+        public void Register()
+        {
+            var registerChat = new ChatMessage() { Command = Command.Register, FromName = _name };
+            _messageSource.SendMessage(registerChat, _serverEndPoint);
+        }
+
+        public void ProcessSendMessage()
+        {
+            while (true)
+            {
+                Console.WriteLine("Input receiver's name");
+                var receiver = ToName;
+                Console.WriteLine("Input your message");
+                var text = Text;
+                var chatMessage = new ChatMessage()
+                { Command = Command.Message, FromName = _name, ToName = receiver, Text = text };
+
+                SendMessage(chatMessage, _serverEndPoint);
+            }
+        }
+
+        public void Listen()
+        {
+            var ip = _messageSource.CreateNewIPEndPoint();
+            Register();
+
+            while (true)
+            {
+                var data = _messageSource.Receive(ref ip);
+                Console.WriteLine($"Сообщение получено от {data.FromName}: \n{data.Text}");
+                Confirmation(data, ip);
+            }
+        }
+
+        public void Confirmation(ChatMessage chatMessage, IPEndPoint ip)
+        {
+            var message = new ChatMessage() { Command = Command.Confirmation, Id = chatMessage.Id };
+            SendMessage(message, ip);
+        }
+
+        public void Start()
+        {
+            new Thread(() => ProcessSendMessage()).Start();
+            Listen();
+        }
+    }
+}
--- NetworkApplicationDevelopmentTesting.Test/UnitTestClient.cs
+++ NetworkApplicationDevelopmentTesting.Test/UnitTestClient.cs
@@ -11,32 +11,46 @@ namespace NetworkApplicationDevelopmentTesting.Test
 {
     public class UnitTestClient
     {
-        private ChatServer _server = new ChatServer(new MessageSource());
-        private int _port = 0;
-        private string _address = "127.0.0.1";
+        //private ChatServer _server = new ChatServer(new MessageSource());
+        //private FakeChatClient first;
+        //private int _port = 0;
+        //private string _address = "127.0.0.1";
 
         [SetUp]
         public void Setup()
         {
-            _server.clients.Clear();
+            //var server = new ChatServer(new MessageSource());
+            //Task.Run(() => server.WorkAsync()).Wait(1000);
+            //first = new FakeChatClient("Alex", new MessageSource(_port, _address));
+            //Task.Run(() => first.Start()).Wait(1000);
         }
 
         [Test] // регистрация на сервере
         public void Test1()
         {
-            Task.Run(() => _server.WorkAsync() ).Wait(1000);
-
-            var сlient = new ChatClient("Alex", new MessageSource(_port, _address));
-            Task.Run(() => сlient.Start()).Wait(1000);
-
-            Task.Run(() => Assert.IsTrue(_server.clients.Count == 1)).Wait(1000);
+            var server = new ChatServer(new MessageSource());
+            Task.Run(() => server.WorkAsync()).Wait(1000);
+            var first = new FakeChatClient("Alex", new MessageSource(0, "127.0.0.1"));
+            Task.Run(() => first.Start()).Wait(1000);
+            Assert.IsTrue(server.clients.Count == 1);
         }
 
 
         [Test] // 
         public void Test2()
         {
-            Task.Run(() => _server.WorkAsync()).Wait(1000);
+            var server1 = new ChatServer(new MessageSource());
+            Task.Run(() => server1.WorkAsync()).Wait(1000);
+
+            var first = new FakeChatClient("Alex", new MessageSource(0, "127.0.0.1"));
+            Task.Run(() => first.Start()).Wait(1000);
+
+            var second = new FakeChatClient("Ivan", new MessageSource(0, "127.0.0.1"));
+            second.ToName = "Alex";
+            second.Text = "Hello Alex";
+            Task.Run(() => second.Start()).Wait(1000);
+
+            
 
 
         }
@@ -45,7 +59,7 @@ namespace NetworkApplicationDevelopmentTesting.Test
         [TearDown]
         public void Teardown()
         {
-            _server.clients.Clear();
+         
         }
     }
 }
